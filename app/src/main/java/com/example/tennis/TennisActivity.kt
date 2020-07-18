b342aabaec5a647c94fcf4f63ed42e302fb9dacd
Sam <supersonicboom7@gmail.com>
2020-07-18 13:52:56 +0100

    Implemented basic UI for application

--- app/src/main/java/com/example/tennis/TennisActivity.kt
+++ app/src/main/java/com/example/tennis/TennisActivity.kt
@@ -2,11 +2,13 @@ package com.example.tennis
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_tennis.*
 
 class TennisActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_tennis)
+        layoutInflater.inflate(R.layout.layout_tennis, game_container)
     }
 }
